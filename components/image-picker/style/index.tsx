e8d2f0c8c0c64f1f6a6f6a813bd1b9702fa6ac9b
张亚飞 <zhangyafei@sishuxuefu.com>
2020-11-25 14:30:21 +0800

    feat: 图片选择添加视频播放按钮

--- components/image-picker/style/index.tsx
+++ components/image-picker/style/index.tsx
@@ -1,24 +1,25 @@
-import { ImageStyle, StyleSheet, TextStyle, ViewStyle } from 'react-native';
-import varibles from '../../style/themes/default';
+import { ImageStyle, StyleSheet, TextStyle, ViewStyle } from "react-native"
+import varibles from "../../style/themes/default"
 
 export interface ImagePickerStyle {
-  container: ViewStyle;
-  size: ImageStyle;
-  item: ViewStyle;
-  image: ImageStyle;
-  closeWrap: ViewStyle;
-  closeText: TextStyle;
-  plusWrap: ViewStyle;
-  plusWrapNormal: ViewStyle;
-  plusWrapHighlight: ViewStyle;
-  plusText: TextStyle;
+  container: ViewStyle
+  size: ImageStyle
+  item: ViewStyle
+  image: ImageStyle
+  closeWrap: ViewStyle
+  closeText: TextStyle
+  plusWrap: ViewStyle
+  plusWrapNormal: ViewStyle
+  plusWrapHighlight: ViewStyle
+  plusText: TextStyle
+  play: ImageStyle
 }
 
 export default () =>
   StyleSheet.create<ImagePickerStyle>({
     container: {
-      flexWrap: 'wrap',
-      flexDirection: 'row',
+      flexWrap: "wrap",
+      flexDirection: "row",
     },
     size: {
       width: 80,
@@ -27,37 +28,37 @@ export default () =>
     item: {
       marginRight: varibles.h_spacing_sm,
       marginBottom: varibles.v_spacing_sm,
-      overflow: 'hidden',
+      overflow: "hidden",
     },
     image: {
-      overflow: 'hidden',
+      overflow: "hidden",
       borderRadius: varibles.radius_sm,
     },
     closeWrap: {
       width: 16,
       height: 16,
-      backgroundColor: '#999',
+      backgroundColor: "#999",
       borderRadius: 8,
-      position: 'absolute',
+      position: "absolute",
       top: 4,
       right: 4,
-      justifyContent: 'center',
-      alignItems: 'center',
-      overflow: 'hidden',
+      justifyContent: "center",
+      alignItems: "center",
+      overflow: "hidden",
     },
     closeText: {
       color: varibles.color_text_base_inverse,
-      backgroundColor: 'transparent',
+      backgroundColor: "transparent",
       fontSize: 20,
       height: 20,
       marginTop: -8,
-      fontWeight: '300',
+      fontWeight: "300",
     },
     plusWrap: {
       borderRadius: varibles.radius_sm,
       borderWidth: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      justifyContent: "center",
+      alignItems: "center",
     },
     plusWrapNormal: {
       backgroundColor: varibles.fill_base,
@@ -69,8 +70,16 @@ export default () =>
     },
     plusText: {
       fontSize: 64,
-      backgroundColor: 'transparent',
-      fontWeight: '100',
+      backgroundColor: "transparent",
+      fontWeight: "100",
       color: varibles.color_text_caption,
     },
-  });
+    play: {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      width: 40,
+      height: 40,
+      transform: [{ translateX: -20 }, { translateY: -20 }],
+    },
+  })
