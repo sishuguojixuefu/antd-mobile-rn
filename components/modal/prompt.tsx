c1e63024e65e72bedc821c6c044a50761439165f
张亚飞 <zhangyafei@sishuxuefu.com>
2020-12-16 10:56:19 +0800

    Revert "feat: prompt修改"
    This reverts commit 6dc61bdbbb4840df1177d81bed6c1423bba4c59c.

--- components/modal/prompt.tsx
+++ components/modal/prompt.tsx
@@ -1,24 +1,23 @@
-import React from "react"
-import { TextStyle } from "react-native"
-import Portal from "../portal"
-import PromptContainer from "./PromptContainer"
-import { CallbackOnBackHandler, CallbackOrActions } from "./PropsType"
+import React from 'react';
+import { TextStyle } from 'react-native';
+import Portal from '../portal';
+import PromptContainer from './PromptContainer';
+import { CallbackOnBackHandler, CallbackOrActions } from './PropsType';
 
 export default function prompt(
   title: React.ReactNode,
   message: React.ReactNode,
   callbackOrActions: CallbackOrActions<TextStyle>,
-  type = "default",
-  defaultValue = "",
-  placeholders = ["", ""],
+  type = 'default',
+  defaultValue = '',
+  placeholders = ['', ''],
   maxLength?: number,
   onBackHandler?: CallbackOnBackHandler,
-  multiline?: boolean
 ) {
   if (!callbackOrActions) {
     // tslint:disable-next-line:no-console
-    console.error("Must specify callbackOrActions")
-    return
+    console.error('Must specify callbackOrActions');
+    return;
   }
 
   const key = Portal.add(
@@ -30,13 +29,12 @@ export default function prompt(
       defaultValue={defaultValue}
       onAnimationEnd={(visible: boolean) => {
         if (!visible) {
-          Portal.remove(key)
+          Portal.remove(key);
         }
       }}
       placeholders={placeholders}
       maxLength={maxLength}
       onBackHandler={onBackHandler}
-      multiline={multiline}
-    />
-  )
+    />,
+  );
 }
